d0af1ef9e782f6d0d6ee30be6d510aec9f7b842d
rolandtrieb <119015836+rolandtrieb@users.noreply.github.com>
2025-03-31 16:53:41 +0200

    Update TrumaSensor.cpp

--- components/truma_inetbox/sensor/TrumaSensor.cpp
+++ components/truma_inetbox/sensor/TrumaSensor.cpp
@@ -25,10 +25,11 @@ void TrumaSensor::setup() {
       //case TRUMA_SENSOR_TYPE::HEATING_MODE:
       //  this->publish_state(static_cast<float>(status_alde->heater_mode));
       //  break;
-      case TRUMA_SENSOR_TYPE::ELECTRIC_POWER_LEVEL:
+      case TRUMA_SENSOR_TYPE::ELECTRIC_POWER_LEVEL: {
         float el_mode = status_alde->el_mode * 100;
         this->publish_state(el_mode);
         break;
+      }
       case TRUMA_SENSOR_TYPE::ENERGY_MIX:
         this->publish_state(static_cast<float>(status_alde->gas_mode));
         break;
