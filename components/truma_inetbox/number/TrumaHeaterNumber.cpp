74291926c56db28b0bc6a96ee616c63c67a04c01
rolandtrieb <119015836+rolandtrieb@users.noreply.github.com>
2025-04-01 09:03:54 +0200

    Update TrumaHeaterNumber.cpp

--- components/truma_inetbox/number/TrumaHeaterNumber.cpp
+++ components/truma_inetbox/number/TrumaHeaterNumber.cpp
@@ -8,16 +8,16 @@ namespace truma_inetbox {
 static const char *const TAG = "truma_inetbox.heater_number";
 
 void TrumaHeaterNumber::setup() {
-  this->parent_->get_heater()->add_on_message_callback([this](const StatusFrameHeater *status_heater) {
+  this->parent_->get_alde_status()->add_on_message_callback([this](const StatusFrameAldeStatus *status_alde) {
     switch (this->type_) {
       case TRUMA_NUMBER_TYPE::TARGET_ROOM_TEMPERATURE:
         this->publish_state(temp_code_to_decimal(status_heater->target_temp_room, 0));
         break;
       case TRUMA_NUMBER_TYPE::TARGET_WATER_TEMPERATURE:
-        this->publish_state(temp_code_to_decimal(status_heater->target_temp_water, 0));
+        this->publish_state(temp_code_to_decimal(water_mode, 0));
         break;
       case TRUMA_NUMBER_TYPE::ELECTRIC_POWER_LEVEL:
-        this->publish_state(static_cast<float>(status_heater->el_power_level_a));
+        this->publish_state(static_cast<float>(status_heater->el_mode));
         break;
       default:
         break;
@@ -28,13 +28,13 @@ void TrumaHeaterNumber::setup() {
 void TrumaHeaterNumber::control(float value) {
   switch (this->type_) {
     case TRUMA_NUMBER_TYPE::TARGET_ROOM_TEMPERATURE:
-      this->parent_->get_heater()->action_heater_room(static_cast<u_int8_t>(value));
+      this->parent_->get_alde_status()->action_heater_room(static_cast<u_int8_t>(value));
       break;
     case TRUMA_NUMBER_TYPE::TARGET_WATER_TEMPERATURE:
-      this->parent_->get_heater()->action_heater_water(static_cast<u_int8_t>(value));
+      this->parent_->get_alde_status()->action_heater_water(static_cast<u_int8_t>(value));
       break;
     case TRUMA_NUMBER_TYPE::ELECTRIC_POWER_LEVEL:
-      this->parent_->get_heater()->action_heater_electric_power_level(static_cast<u_int16_t>(value));
+      this->parent_->get_alde_status()->action_heater_electric_power_level(static_cast<u_int16_t>(value));
       break;
     default:
       break;
