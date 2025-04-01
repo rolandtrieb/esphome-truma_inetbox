907537fac96a1c63cbe0d71b044a1d2c22922af0
rolandtrieb <119015836+rolandtrieb@users.noreply.github.com>
2025-04-01 17:26:37 +0200

    Update TrumaSensor.cpp

--- components/truma_inetbox/sensor/TrumaSensor.cpp
+++ components/truma_inetbox/sensor/TrumaSensor.cpp
@@ -14,7 +14,7 @@ void TrumaSensor::setup() {
         this->publish_state(temp_code_to_decimal(status_alde->current_temp_inside));
         break;
       case TRUMA_SENSOR_TYPE::CURRENT_WATER_TEMPERATURE:
-        this->publish_state(temp_code_to_decimal(status_alde->unknown_23));
+        this->publish_state(temp_code_to_decimal(status_alde->unknown_24));
         break;
       case TRUMA_SENSOR_TYPE::TARGET_ROOM_TEMPERATURE:
         this->publish_state(temp_code_to_decimal(status_alde->target_temp_room));
